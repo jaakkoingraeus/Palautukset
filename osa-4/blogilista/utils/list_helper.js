d19a764f357a74bacfa6f04e2bbd8535e2513c4f
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-04-14 17:16:35 +0300

    4.4

--- osa-4/blogilista/utils/list_helper.js
+++ osa-4/blogilista/utils/list_helper.js
@@ -1,7 +1,20 @@
+const app = require("../app")
+
 const dummy = (blogs) => {
-  return 1
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else if (blogs.length === 1) {
+        return blogs[0].likes
+    } else {
+        return blogs.map( (n) => n.likes).reduce( (a, b) => a + b )
+    }
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
