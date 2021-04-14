7f579fe960971ac11cc7c417c17a133110437f72
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-04-14 16:05:13 +0300

    Working deconstructed version

--- osa-4/blogilista/app.js
+++ osa-4/blogilista/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
   
@@ -26,7 +27,11 @@ mongoose
   
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
   
 module.exports = app
