9de91c060250196751dedfbc18872400f4b11a73
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-04-14 15:22:20 +0300

    Working app.js

--- osa-4/blogilista/index.js
+++ osa-4/blogilista/index.js
@@ -1,57 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://jakkepoko:jakke_poko99@cluster0.wdafa.mongodb.net/blogilista?retryWrites=true&w=majority'
-
-mongoose
-  .connect(mongoUrl, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false, 
-    useCreateIndex: true 
-  })
-  .then( res => {
-    logger.info("Connected to MongoDB")
-  })
-  .catch( err => {
-    logger.error("Error connecting to MongoDB")
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
