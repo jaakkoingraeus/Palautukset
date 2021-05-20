11a5d81bc0a12e8469d40cb4f59e1914665d81ff
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-05-20 17:34:21 +0300

    Token request working

--- osa-4/blogilista/app.js
+++ osa-4/blogilista/app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
   
 const mongoUrl = config.MONGODB_URI
 
@@ -31,6 +32,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login/', loginRouter)
 app.use('/api/users/', usersRouter)
 app.use('/api/blogs/', blogsRouter)
 
