6f132dac8d7799487b3f59a4daef1dc23ca3fcb1
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-05-26 12:43:45 +0300

    5.3

--- osa-5/bloglist-frontend/src/App.js
+++ osa-5/bloglist-frontend/src/App.js
@@ -11,11 +11,10 @@ const App = () => {
   const [password, setPassword] = useState("")
   const [blogToAdd, setBlogToAdd] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  //New Blog
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
   useEffect(() => {
     const user = window.localStorage.getItem('user')
@@ -24,12 +23,17 @@ const App = () => {
     }
     const userJSON = JSON.parse(user)
     setUser(userJSON)
+    blogService.setToken(userJSON.token)
   }, [])
 
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
-    
     try {
       const user = await loginService.login({ 
         username, 
@@ -41,6 +45,7 @@ const App = () => {
       //Add user to local storage
       const userString = JSON.stringify(user)
       window.localStorage.setItem('user', userString)
+      blogService.setToken(user.token)
     }
     catch {
       console.log('invalid credentials')
@@ -50,11 +55,6 @@ const App = () => {
   const logOut = () => {
     window.localStorage.removeItem('user')
     window.location.reload()
-  }
-
-  const handleAddBlog = (event) => {
-    event.preventDefault()
-
   }
 
   const loginForm = () => {
@@ -84,15 +84,63 @@ const App = () => {
     )
   }
 
+  //ADD BLOG
+  const blogSubmit = (e) => {
+    e.preventDefault()
+    const elem = e.target
+    const blog = {
+      title: elem.blogTitle.value,
+      author: elem.blogAuthor.value,
+      url: elem.blogUrl.value
+    }
+    console.log('Adding blog: ', blog)
+
+    blogService.addBlog(blog, user)
+    setNewUrl('')
+    setNewTitle('')
+    setNewAuthor('')
+  }
+
   const blogsSection = () => {
     return (
       <>
+      <div>
+        <h2>Add new blog</h2>
+        <form onSubmit={blogSubmit}>
+          <div>
+          Title:
+          <input
+          type = "text"
+          value = {newTitle}
+          name = "blogTitle"
+          onChange = {({ target }) => setNewTitle(target.value)}/>
+          </div>
+          <div>
+          Author:
+          <input
+          type = "text"
+          value = {newAuthor}
+          name = "blogAuthor"
+          onChange = {({ target }) => setNewAuthor(target.value)}/>
+          </div>
+          <div>
+          URL:
+          <input
+          type = "text"
+          value = {newUrl}
+          name = "blogUrl"
+          onChange = {({ target }) => setNewUrl(target.value)}/>
+          </div>
+          <button type="submit">Add blog</button>
+        </form>
+      </div>
+
       <div>
         <h2>Logged in as {user.name}</h2>
         <button onClick={logOut}>Log out</button>
         <h2>Blogs</h2>
         {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
+            <Blog key={blog.id} blog={blog}/>
         )}
       </div>
       </>
